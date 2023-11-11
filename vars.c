168aa4e6064c3c5c1667d9362e2d0792d000a6a9
Innocentem <mwbzinno@gmail.com>
2023-11-11 00:10:43 -0800

    vars

--- vars.c
+++ vars.c
@@ -10,29 +10,30 @@
  */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
-	size_t j = *p;
+    size_t j = *p;
 
-	if (buf[j] == '|' && buf[j + 1] == '|')
-	{
-		buf[j] = 0;
-		j++;
-		info->cmd_buf_type = CMD_OR;
-	}
-	else if (buf[j] == '&' && buf[j + 1] == '&')
-	{
-		buf[j] = 0;
-		j++;
-		info->cmd_buf_type = CMD_AND;
-	}
-	else if (buf[j] == 'NULL')
-	{
-		buf[j] = 0;
-		info->cmd_buf_type = CMD_CHAIN;
-	}
-	else
-		return (0);
-	*p = j;
-	return (1);
+    if (buf[j] == '|' && buf[j + 1] == '|')
+    {
+        buf[j] = 0;
+        j++;
+        info->cmd_buf_type = CMD_OR;
+    }
+    else if (buf[j] == '&' && buf[j + 1] == '&')
+    {
+        buf[j] = 0;
+        j++;
+        info->cmd_buf_type = CMD_AND;
+    }
+    else if (strcmp(&buf[j], "NULL") == 0)
+    {
+        buf[j] = 0;
+        info->cmd_buf_type = CMD_CHAIN;
+    }
+    else
+        return 0;
+
+    *p = j;
+    return 1;
 }
 
 /**
