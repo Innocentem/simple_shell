13c04070cac0d21bfe7a0b1c20034fe5430ae726
Innocentem <mwbzinno@gmail.com>
2023-11-16 13:21:06 +0300

    hstry

--- history.c
+++ history.c
@@ -8,17 +8,17 @@
 
 char *get_history_file(info_t *info)
 {
-    char *buf, *dir;
+    char *buf, *info;
 
     dir = getenv("HOME");
-    if (!dir)
+    if (!info)
         return (NULL);
 
     buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
     if (!buf)
         return (NULL);
     buf[0] = 0;
-    _strcpy(buf, dir);
+    _strcpy(buf, info);
     _strcat(buf, "/");
     _strcat(buf, HIST_FILE);
     return (buf);
