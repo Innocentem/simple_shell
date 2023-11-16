beb9ba7e59f586f255eb375e629800630787c4ed
Innocentem <mwbzinno@gmail.com>
2023-11-16 01:58:13 -0800

    history

--- history.c
+++ history.c
@@ -8,22 +8,21 @@
 
 char *get_history_file(info_t *info)
 {
-	char *buf, *dir;
+    char *buf, *dir;
 
-	dir = getenv("HOME");
-	if (!dir)
-		return (NULL);
+    dir = getenv("HOME");
+    if (!dir)
+        return (NULL);
 
-	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
-	if (!buf)
-		return (NULL);
-	buf[0] = 0;
-	_strcpy(buf, dir);
-	_strcat(buf, "/");
-	_strcat(buf, HIST_FILE);
-	return (buf);
+    buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+    if (!buf)
+        return (NULL);
+    buf[0] = 0;
+    _strcpy(buf, dir);
+    _strcat(buf, "/");
+    _strcat(buf, HIST_FILE);
+    return (buf);
 }
-
 /**
  * write_history: to create a file / append to an existing file
  * @info: struct parameter.
