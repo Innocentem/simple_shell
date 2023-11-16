f3b8c4506becec4f0f66ade803aa2cf04f0e35e1
Innocentem <mwbzinno@gmail.com>
2023-11-16 13:29:32 +0300

    hstry

--- history.c
+++ history.c
@@ -14,7 +14,7 @@ char *get_history_file(info_t *info)
     if (!info)
         return (NULL);
 
-    buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+    buf = malloc(sizeof(char) * (_strlen(info) + _strlen(HIST_FILE) + 2));
     if (!buf)
         return (NULL);
     buf[0] = 0;
